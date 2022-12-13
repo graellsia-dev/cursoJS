eb41938691817644ecc5d6d7acd88785b34c7509
Curso 2 <nnnats9@gmail.com>
2022-12-13 13:54:07 +0100

    Ejercicio grados

---
+++ natalia/02-javascript/script.js
@@ -0,0 +1,52 @@
+// console.log("Hola mundo");
+
+const saludo = "Hola clase, soy Natalia";
+
+// console.log(saludo);
+
+const saludo2 = "Hola clase";
+console.log(saludo2 + " soy Natalia")
+
+let saludo3 = "hola clase";
+console.log(saludo3);
+saludo3 = "Hola clase soy Natalia";
+
+console.log(saludo3);
+
+//ejercicios: crear 3 variables una modificable.
+//hacer una suma, una resta y multiplicación de las 3 variables.
+//luego actualizar la tercera y volver a hacer la multiplicación
+
+const a = 21;
+const b = 2;
+let c = 4;
+
+
+console.log (a+b);
+console.log (a-b);
+console.log (a*b*c);
+
+c = 10; // Esto es una actualización de la variable.
+console.log (a+b);
+console.log (a-b);
+console.log (a*b*c);
+
+ // console.warn("Esto es un aviso");
+
+ //const resAlert = alert("esta página");
+
+ //console.log (resAlert);
+
+ const resPrompt = prompt("¿Qué temperatura hace?");
+
+ console.log(parseInt (resPrompt) +273);
+
+ console.log(typeof resPrompt);
+ console.log(typeof parseInt(resPrompt));
+
+
+
+
+
+
+
