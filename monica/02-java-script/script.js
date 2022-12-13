14a08bb5a3ec64dae5c586a9aa78c1fabad543a5
monicajava22 <mbkf.cinco@gmail.com>
2022-12-13 14:03:34 +0100

    ejercicios

---
+++ monica/02-java-script/script.js
@@ -0,0 +1,58 @@
+
+
+// console.log('Hola mundo')
+
+// console.log('Holamundo');
+
+const saludo = "Hola mundo "
+
+// console.log(saludo);
+
+const saludo2 = "Hola clase";
+
+// console.log(saludo2 + " soy monica");
+let saludo3 = "hola clase";
+
+// console.log(saludo3);
+
+saludo3= "Hola clase soy Jorge";
+
+console.log(saludo3);
+
+// Ejercicios: crear tres variables, una modificable.
+// hacer una suma, una resta, una multiplicación de las 3 variables,
+//luego actualizar la tercera y volver a hacer la multiplicación
+
+const a = 10;
+const b = 15;
+let c = 12;
+
+// console.log( "Suma " , a + b + c );
+// console.log( "Resta " , a - b - c );
+// console.log( "Multiplicación " , a * b * c);
+
+c = -5; // Esto es una actualización de la variable.
+
+// console.log( "Suma " , a + b + c);
+// console.log( "Resta " , a - b - c);
+// console.log( "Multiplicacon ", a * b* c);
+
+// console.warm('Esto es un error');
+// console.error('Esto es un error grave');
+
+// const resAlert = alert('Esta pagina es para mayores')
+
+// console.log(resAlert);
+
+const resPrompt = prompt('¿Que temperatura hace?');
+
+// console.log("Temp en K: ",resPrompt + 273);
+
+// console.log( parseInt( resPrompt ) + 273);
+
+console.log(typeof resPrompt);
+console.log(typeof parseInt( resPrompt ));
+
+
+
+
