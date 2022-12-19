01877499a7722dfd56448b91dce4c5725c903507
illunne <evablazquezvega@gmail.com>
2022-12-19 11:51:53 +0100

    tema3

--- illunne/03-tipos-de-datos/script.js
+++ illunne/03-tipos-de-datos/script.js
@@ -3,31 +3,63 @@
 // Las herramientas de desarrollador deberán estar siempre abiertas.
 // El codigo deberá organizarse de manera limpia y lógica utilizando siempre las buenas practicas indicadas en clase.
 
+ console.log("Tercer dia día de clase")
+
+
 // Ejercicio 1:
-// Crear una variable de cada tipo de los que hemos visto en clase (string, number, undefined, array, booleano)
+// Crear una variable de cada tipo de los que hemos visto en clase (string, number, undefined, booleano)
 // Cada variable deberemos aplicarle el metodo correspondiente que devuelva cual es el tipo de dato
 
+// let variableString = "Hola, buenos días.";
+// console.log(variableString);
+// console.log(variableString, typeof variableString);
 
+// let variableNumber = 456;
+// console.log(variableNumber);
+// console.log(variableNumber, typeof variableNumber);
+
+// let variableUndefined;
+// console.log(variableUndefined);
+// console.log(variableUndefined, typeof variableUndefined);
+
+// let variableBooleano = true;
+// console.log(variableBooleano);
+// console.log(variableBooleano, typeof variableBooleano);
 
 
 
 // Ejercicio 2: 
 // Crear una variable tipo string que contenga un numero, convertir esta variable a numero.
 
+ let variableString2 = "520";
+console.log('tipo', typeof variableString2);
+
+variableString2 = parseInt(variableString2)
+console.log('tipo', typeof variableString2);
 
 
+// variableStringToNumber= 520;
+// console.log(variableStringToNumber, typeof variableStringToNumber);
 
 
-// Ejercicio 3: 
+// Ejercicio 3: AVANZADO
 // Crear una variable que sea un numero decimal, obtener su parte entera y su parte decimal por separado, 
 // y guardarlo en dos variables por separado
 
+let numeroDecimal = 5.95;
+console.log(parseInt(numeroDecimal))
+console.log(Math.floor(numeroDecimal))
+
+console.log((numeroDecimal - Math.floor(numeroDecimal)).toFixed(2));
 
 
 
 
-// Ejercicio 4:
+
+// Ejercicio 4: AVANZADO
 // Hacer una operación matemática que devuelva NaN.
+
+console.log(3*"hola");
 
 
 
@@ -36,11 +68,13 @@
 // Ejercicio 6:
 // Mediante un prompt solicitar al usuario que introduzca una temperatura y cambiarla a grados farenheit.
 
+const temperatura = prompt
 
 
 
 
-// Ejercicio 7:
+
+// Ejercicio 7: AVANZADO
 // Mediante un prompt solicitar al usuario 3 numeros que formen una ecuacion de segundo grado y resolverla.
 
 
