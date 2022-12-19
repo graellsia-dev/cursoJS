5b7682c97320be3b650f022b6df91ff3870e474f
RosmaSF <rosmasantos@gmail.com>
2022-12-19 14:04:06 +0100

    lunes

---
+++ rosma/04-metodos/script.js
@@ -0,0 +1,89 @@
+
+
+
+
+
+// Ejercicios:
+// para abrir el html voy a la carpeta del local
+// El alumno deberá linkar el archivo script.js con el archivo index.html para utilizarlo desde el navegador.
+// Las herramientas de desarrollador deberán estar siempre abiertas.
+// El codigo deberá organizarse de manera limpia y lógica utilizando siempre las buenas practicas indicadas en clase.
+
+// Ejercicio 1:
+// El alumno deberá crear un array con mas de dos posiciones y mediante el metodo adecuado mostrará el tamaño del array.
+// El alumno modificará el tamaño del array y volverá a comprobar que devuelve el metodo
+// fila 26 me devuelve la posición 1 de la array
+// fila 27 me devuelve ultima posicion
+
+let array = ["rosma" , 25 , 10 , "pau"];
+console.log(array.length);
+
+const array2 = ["hola" , "adios" , 25];
+
+
+array.push(true);
+
+console.log(array[1]); 
+console.log(array[array.length-1]);
+
+
+
+// Ejercicio 2:
+// El alumno deberá crear un string con mas de dos caracteres y mediante el metodo adecuado mostrará el tamaño del string.
+// El alumno modificará el tamaño del string y volverá a comprobar que devuelve el metodo
+
+let variableString5 =  "cable   ";
+console.log(variableString5.length);
+
+
+let variableString2 =  "cable";
+console.log(variableString2.length);
+
+
+
+// Ejercicio 3: 
+// El alumno creará un string en minúsculas y mediante el metodo adecuado lo pasará a mayúsculas.
+
+console.log("saludos".toUpperCase());
+
+
+
+
+// Ejercicio 4: 
+// El alumno creará un string en mayúsculas y mediante el metodo adecuado lo pasará a minúsculas.
+
+console.log("NOS VAMOS".toLowerCase());
+
+// Ejercicio 5: 
+// El alumno deberá obtener el elemento de la posición 2 y 4 del siguiente array: const array = ['hola', 12, , true, [ 1, 3], 'mundo']
+
+
+
+// Ejercicio 6:
+// El alumno añadirá nuevos elementos al array en la ultima posición.
+
+
+
+// Ejercicio 7: AVANZADO
+// El alumno mediante el método adecuado pregunta si un array de numeros dado incluye un número "3"
+// const arryNumbers = [1,2,3,4,5]
+// const arryNumbers2 = [2,4,6,8,10]
+
+
+
+
+// Ejercicio 8: AVANZADO
+// El alumno mostrará por consola un hola mundo utilizando los elementos del array del ejercicio 5.
+
+
+
+
+
+// Ejercicio 9: AVANZADO
+// El alumno dado un array de numeros devolverá un array con todos los términos multiplicados por dos.
+
+
+
+// Ejercicio 10: AVANZADO
+// El alumno obtendrá la ultima posición de cualquier arrray.
+
