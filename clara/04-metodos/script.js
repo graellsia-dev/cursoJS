acb01428bd545295bf452ca6d2a2d7e10ee855b5
clara <info@aclararte.egit config --global user.email info@aclararte.ess>
2022-12-19 11:50:35 +0100

    tema 3

---
+++ clara/04-metodos/script.js
@@ -0,0 +1,67 @@
+// Ejercicios:
+// El alumno deberá linkar el archivo script.js con el archivo index.html para utilizarlo desde el navegador.
+// Las herramientas de desarrollador deberán estar siempre abiertas.
+// El codigo deberá organizarse de manera limpia y lógica utilizando siempre las buenas practicas indicadas en clase.
+console.log("Comenzamos los ejercicios");
+
+
+
+
+// Ejercicio 1:
+// El alumno deberá crear un array con mas de dos posiciones y mediante el metodo adecuado mostrará el tamaño del array.
+// El alumno modificará el tamaño del array y volverá a comprobar que devuelve el metodo
+
+
+
+
+// Ejercicio 2:
+// El alumno deberá crear un string con mas de dos caracteres y mediante el metodo adecuado mostrará el tamaño del string.
+// El alumno modificará el tamaño del string y volverá a comprobar que devuelve el metodo
+
+
+
+
+// Ejercicio 3: 
+// El alumno creará un string en minúsculas y mediante el metodo adecuado lo pasará a mayúsculas.
+
+
+
+
+// Ejercicio 4: 
+// El alumno creará un string en mayúsculas y mediante el metodo adecuado lo pasará a minúsculas.
+
+
+
+// Ejercicio 5: 
+// El alumno deberá obtener el elemento de la posición 2 y 4 del siguiente array: const array = ['hola', 12, , true, [ 1, 3], 'mundo']
+
+
+
+// Ejercicio 6:
+// El alumno añadirá nuevos elementos al array en la ultima posición.
+
+
+
+// Ejercicio 7: AVANZADO
+// El alumno mediante el método adecuado pregunta si un array de numeros dado incluye un número "3"
+// const arryNumbers = [1,2,3,4,5]
+// const arryNumbers2 = [2,4,6,8,10]
+
+
+
+
+// Ejercicio 8: AVANZADO
+// El alumno mostrará por consola un hola mundo utilizando los elementos del array del ejercicio 5.
+
+
+
+
+
+// Ejercicio 9: AVANZADO
+// El alumno dado un array de numeros devolverá un array con todos los términos multiplicados por dos.
+
+
+
+// Ejercicio 10: AVANZADO
+// El alumno obtendrá la ultima posición de cualquier arrray.
+
