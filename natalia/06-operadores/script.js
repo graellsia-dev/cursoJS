5392dac104bc5c6a17a88a7216669d52447a58e5
Curso 2 <nnnats9@gmail.com>
2022-12-20 14:03:13 +0100

    tema 4

---
+++ natalia/06-operadores/script.js
@@ -0,0 +1,68 @@
+// Ejercicios:
+// El alumno deberá linkar el archivo script.js con el archivo index.html para utilizarlo desde el navegador.
+// Las herramientas de desarrollador deberán estar siempre abiertas.
+// El codigo deberá organizarse de manera limpia y lógica utilizando siempre las buenas practicas indicadas en clase.
+
+// Ejercicio 1: 
+// Sumar, restar, multiplicar, elevar al cuadrado, hacer la raíz cuadrada y dividir numeroA y numeroB.
+let numeroA = 36
+let numeroB = 16
+
+let suma = numeroA + numeroB
+console.log(suma)
+let resta = numeroA - numeroB
+console.log(resta)
+let multiplicar= numeroA*numeroB
+console.log(multiplicar)
+let elevar= numeroA** numeroB
+console.log( elevar)
+
+let raiz1 = Math.sqrt(numeroA) 
+let raiz2 = Math.sqrt(numeroB) 
+let dividir = numeroA/numeroB
+
+
+// Ejercicio 2:
+// Hacer que numeroA aumente en dos unidades dos veces.
+let numero = 36
+numero = numero + 2
+numero +=2
+
+console.log(numero)
+
+// Ejercicio 3:
+// Obtener el resto de la división entre numeroA y numeroB y multiplicarlo por 5
+let numeroA2 = 4
+let numeroB3 = 2
+let resto = 4%2
+
+
+
+// Ejercicio 6: 
+// Debemos sumar todos los numeros de el siguiente array.
+let array = [2, 5, 8, 15]
+
+let sumaArr = array.forEach(element => suma = suma + element)
+
+console.log (let sumaArr) 
+// Ejercicio 6: 
+// Mediante un prompt se solicita un nombre al usuario. Mediate otro prompt se solicita la edad usuario. Si se llama Jorge o Cristian y además tiene mas de 18 años se mostrará un alert que poga "El usuario tiene acceso". 
+let nombre = prompt('nombre')
+let edad = prompt ('edad')
+
+console.log 
+if (c) {
+    
+} else {
+    
+}
+
+// Ejercicio 4: AVANZADO
+// Mediante un prompt se solicita un numero al usuario y este deberá decir si es par o impar.
+
+
+// Ejercicio 5: AVANZADO
+// En el ejercicio anterior si el usuario no introduce un numero, deberá aparecer un alert que diga "Debe introducir un numero" 
+
+
+
